7a6148122be98ad1c3aca0452729102dc8a9b626
megared <mredikf@gmail.com>
2024-09-03 13:10:25 +0300

    Added online chart

--- backend/routes/api/v1/minecraft.py
+++ backend/routes/api/v1/minecraft.py
@@ -1,11 +1,10 @@
 from typing import Annotated
+from datetime import datetime
 
-from fastapi import APIRouter, Depends, HTTPException
-from httpx import get, post, delete
-from starlette.responses import Response
+from fastapi import APIRouter, HTTPException, Depends
+from httpx import get
 
 from backend import schemas
-from backend.routes.api.v1.discord import auth_middleware
 from backend.routes.api.v1.discord.auth import only_moderator
 from backend.schemas import DiscordGuildMember
 from backend.utils import config
@@ -15,48 +14,32 @@ from backend.utils.minecraft import ServerEnum, SERVERS
 router = APIRouter(prefix="/minecraft", tags=["minecraft"])
 
 
-@router.get(
-    "/{server}/money",
-    response_model=schemas.MCUserData,
-)
-def get_money(
-    server: ServerEnum,
-    discord_user: Annotated[DiscordGuildMember, Depends(auth_middleware)],
-):
-    username = str(discord_user.nick)
-    server_host = SERVERS[server]
-
+@router.get("/online")
+def get_online_count():
     resp = get(
-        "%s/money/%s" % (server_host, username),
-        headers={"Authorization": config.MC_SERVER_KEY},
-    )
-
-    if resp.status_code != 200:
-        raise HTTPException(404, detail="Minecraft user not found.")
-
-    user_data = schemas.MCUserData(username=username, money=resp.json().get("money"))
-
-    return user_data
-
-
-@router.get(
-    "/{server}/online",
-    response_model=schemas.MCOnline,
-)
-def get_online_count(
-    server: ServerEnum,
-):
-    server_host = SERVERS[server]
-
-    resp = get(
-        "%s/online" % server_host,
-        headers={"Authorization": config.MC_SERVER_KEY},
+        f"{config.PROXY_SERVER_HOST}/api/getOnline?secret_key={config.PROXY_SERVER_KEY}",
     )
 
     if resp.status_code != 200:
         raise HTTPException(404, detail="Minecraft server not found.")
 
-    online_data = schemas.MCOnline(count=resp.json().get("count"))
+    online_data = resp.json()
+
+    online_data["labels"] = [
+        "Понедельник",
+        "Вторник",
+        "Среда",
+        "Четверг",
+        "Пятница",
+        "Суббота",
+        "Воскресенье",
+    ]
+
+    curr_day = datetime.now().isoweekday()
+
+    online_data["labels"] = (
+        online_data["labels"][curr_day:] + online_data["labels"][:curr_day]
+    )
 
     return online_data
 
@@ -71,11 +54,6 @@ async def post_to_whitelist(
 ):
     server_host = SERVERS[server]
 
-    resp = post(
-        "%s/whitelist/%s" % (server_host, username),
-        headers={"Authorization": config.MC_SERVER_KEY},
-    )
-
     msg = schemas.discord.CreateDiscordMessage()
     msg.embeds = [
         {
@@ -86,40 +64,5 @@ async def post_to_whitelist(
         }
     ]
     await send_message(config.DISCORD_LOG_CHANNEL_ID, msg, False)
-
-    if resp.status_code != 200:
-        raise HTTPException(400)
-
-    return Response()
-
-
-@router.delete(
-    "/{server}/whitelist/{username}",
-)
-async def remove_from_whitelist(
-    server: ServerEnum,
-    username: str,
-    guild_member: Annotated[DiscordGuildMember, Depends(only_moderator)],
-):
-    server_host = SERVERS[server]
-
-    resp = delete(
-        "%s/whitelist/%s" % (server_host, username),
-        headers={"Authorization": config.MC_SERVER_KEY},
-    )
-
-    msg = schemas.discord.CreateDiscordMessage()
-    msg.embeds = [
-        {
-            "title": "Изменение белого списка",
-            "description": (
-                f"Пользователь <@{guild_member.user.id}> удаляет из белого списка {username} на сервере {server}"
-            ),
-        }
-    ]
-    await send_message(config.DISCORD_LOG_CHANNEL_ID, msg, False)
-
-    if resp.status_code != 200:
-        raise HTTPException(400)
 
     return Response()
